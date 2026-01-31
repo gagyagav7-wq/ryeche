b28b21c7a9127b337bc8211bd5a504394606b93e
gagyagav7-wq <gagyagav7@gmail.com>
2026-01-31 18:19:39 +0700

    Create prisma-auth.ts

---
+++ src/lib/prisma-auth.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as { prismaAuth: PrismaClient };
+
+// Gunakan 'prismaAuth' biar gak bentrok variable globalnya
+export const prismaAuth = globalForPrisma.prismaAuth || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prismaAuth = prismaAuth;
