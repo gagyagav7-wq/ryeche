b053d314f112d95eb2c866d6a0c794c9444511fd
gagyagav7-wq <gagyagav7@gmail.com>
2026-02-01 03:58:39 +0700

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,12 @@
-import { PrismaClient } from "@prisma/client-movie";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-const dbUrl = process.env.DATABASE_URL;
-
-if (!dbUrl) {
-  throw new Error("DATABASE_URL kosong. Pastikan .env kebaca pas runtime (next start).");
-}
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     datasources: {
-      db: { url: dbUrl },
+      db: { url: process.env.DATABASE_URL },
     },
   });
 
