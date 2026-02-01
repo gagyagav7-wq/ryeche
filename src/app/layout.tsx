6eff465a54dbd0490be75f4fdf004abb2f1e6be3
gagyagav7-wq <gagyagav7@gmail.com>
2026-02-01 16:24:58 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,7 @@ export const metadata: Metadata = {
   description: "Nonton Drama Gaya Brutal",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="font-sans">{children}</body>
